e25cd6f4b1bfa46484c526915bcc86baf6c2bfd0
Risvi-Tareq <ivsirqerat@gmail.com>
2024-01-23 15:24:34 -0800

    apcakge

--- server/config/connection.mjs
+++ server/config/connection.mjs
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+mongoose.set('strictQuery', false);
+
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/prodcollabDB',
   {
@@ -8,7 +10,6 @@ mongoose.connect(
   }
 );
 
-mongoose.set('strictQuery', false);
 
 
 const dbConnection = mongoose.connection;
