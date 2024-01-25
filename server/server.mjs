8fc8062ec3156d80d4c44890e5a9292cd2837288
Risvi-Tareq <ivsirqerat@gmail.com>
2024-01-24 21:49:29 -0800

    Revert "minor changes to dev"
    This reverts commit 21bec416c375285a3b0d2984d4e6c9967bd1d1c6.

--- server/server.mjs
+++ server/server.mjs
@@ -41,9 +41,9 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 
   dbConnection.once("open", () => {
     app.listen(PORT, () => {
