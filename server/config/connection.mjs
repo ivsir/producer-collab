eba50fcfc4db1d9f8abb902e2aaa49fe2fbe495f
Risvi-Tareq <ivsirqerat@gmail.com>
2024-05-20 12:40:22 -0700

    new mongO

--- server/config/connection.mjs
+++ server/config/connection.mjs
@@ -1,23 +1,26 @@
 import mongoose from 'mongoose';
 
+const uri = "mongodb+srv://ivsir:Ulang1411!@producer-collab.dyoeqvd.mongodb.net/?retryWrites=true&w=majority&appName=producer-collab";
+
 mongoose.set('strictQuery', false);
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/prodcollabDB',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
 // mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb+srv://ivsir:Ulang1411!@producer-collab.dyoeqvd.mongodb.net/?retryWrites=true&w=majority&appName=producer-collab',
+//   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/prodcollabDB',
 //   {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
 //   }
 // );
 
+mongoose.connect(
+  process.env.MONGODB_URI || uri,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
 const dbConnection = mongoose.connection;
 
 export default dbConnection;
+
