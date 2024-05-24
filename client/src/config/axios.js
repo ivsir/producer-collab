7366ab76acf03d453bffc689b70c2db58863d26d
Risvi-Tareq <ivsirqerat@gmail.com>
2024-05-24 14:40:27 -0700

    changed the env

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL  || "http://localhost:3000";
+const baseURL = process.env.REACT_APP_API_URL  || "https://gentle-headland-15406-059764510da4.herokuapp.com/";
 
 const axiosClient = axios.create({
   baseURL,
