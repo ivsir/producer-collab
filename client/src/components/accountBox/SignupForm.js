2deef175e70f02ff0e5a9acd6f03ef869c11a765
Risvi-Tareq <ivsirqerat@gmail.com>
2024-05-24 17:32:44 -0700

    revert

--- client/src/components/accountBox/SignupForm.js
+++ client/src/components/accountBox/SignupForm.js
@@ -22,7 +22,7 @@ import {
   SubmitButton,
 } from "../accountBox/Common";
 
-const baseURL = process.env.REACT_APP_API_URL || "https://gentle-headland-15406-059764510da4.herokuapp.com/";
+const baseURL = process.env.REACT_APP_API_URL;
 
 console.log(baseURL)
 export function SignupForm() {
