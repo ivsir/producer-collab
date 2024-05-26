2431f55e20490808c7397f2810c36e173676c0e9
Risvi-Tareq <ivsirqerat@gmail.com>
2024-05-25 17:09:14 -0700

    mesage

--- server/config/connection.mjs
+++ server/config/connection.mjs
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const uri = "mongodb+srv://ivsir:Ulang1411!@producer-collab.dyoeqvd.mongodb.net/?retryWrites=true&w=majority&appName=producer-collab";
-
+// const uri = "mongodb://ivsir:Ulang1411!@docdb-2024-05-25-21-57-11.cluster-cpmy6ykiy74g.us-west-2.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 mongoose.set('strictQuery', false);
 
 // mongoose.connect(
