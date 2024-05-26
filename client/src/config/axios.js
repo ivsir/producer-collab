f7469119663bff07d2fc00ad8975b09738889097
Risvi-Tareq <ivsirqerat@gmail.com>
2024-05-25 17:12:09 -0700

    stored

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL
+const baseURL = process.env.REACT_APP_API_URL || "ec2-54-153-127-238.us-west-1.compute.amazonaws.com"
 
 const axiosClient = axios.create({
   baseURL,
