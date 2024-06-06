954a5ef469a4d5b4e1646e85af26de6e0564098c
Risvi-Tareq <ivsirqerat@gmail.com>
2024-06-06 13:02:43 -0700

    console.log base url

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const baseURL = process.env.REACT_APP_API_URL
 const baseURL = window.location.origin
-
+console.log("base url",baseURL)
 const axiosClient = axios.create({
   baseURL,
 });
