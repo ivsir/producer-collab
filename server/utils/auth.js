5e14b6be2ed0402a45502d518cc794d036c01e32
Risvi-Tareq <ivsirqerat@gmail.com>
2024-06-22 15:58:56 -0700

    added middlewaaer

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,27 +1,59 @@
+// const jwt = require('jsonwebtoken');
+
+// const secret = 'mysecretssshhhhhhh';
+// const expiration = '2h';
+
+// function authMiddleware({ req }) {
+//   let token = req.body.token || req.query.token || req.headers.authorization;
+
+//   if (req.headers.authorization) {
+//     token = token.split(' ').pop().trim();
+//   }
+
+//   if (!token) {
+//     return req;
+//   }
+
+//   try {
+//     const { data } = jwt.verify(token, secret, { maxAge: expiration });
+//     req.user = data;
+//   } catch {
+//     console.log('Invalid token');
+//   }
+
+//   return req;
+// }
+
+// function signToken({ email, username, _id }) {
+//   const payload = { email, username, _id };
+//   return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+// }
+
+// module.exports = {authMiddleware, signToken}
+
 const jwt = require('jsonwebtoken');
 
 const secret = 'mysecretssshhhhhhh';
 const expiration = '2h';
 
-function authMiddleware({ req }) {
-  let token = req.body.token || req.query.token || req.headers.authorization;
+function authMiddleware(event) {
+  let token = event.headers.authorization || event.headers.Authorization;
 
-  if (req.headers.authorization) {
+  if (token) {
     token = token.split(' ').pop().trim();
-  }
-
-  if (!token) {
-    return req;
+  } else {
+    console.log('No token found');
+    return null;
   }
 
   try {
     const { data } = jwt.verify(token, secret, { maxAge: expiration });
-    req.user = data;
-  } catch {
-    console.log('Invalid token');
+    console.log('User data set in request:', data);
+    return data;
+  } catch (error) {
+    console.log('Invalid token', error);
+    return null;
   }
-
-  return req;
 }
 
 function signToken({ email, username, _id }) {
@@ -29,4 +61,4 @@ function signToken({ email, username, _id }) {
   return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
 }
 
-module.exports = {authMiddleware, signToken}
+module.exports = { authMiddleware, signToken };
